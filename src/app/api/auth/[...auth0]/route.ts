8e6ae62efb88b448f50cd9a476ce7ca7343789dc
devonshigaki <devonshigaki@gmail.com>
2025-05-30 18:24:44 -0700

    fix: Resolve dependency conflicts and add missing configs

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -1,4 +1,9 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    returnTo: '/connect'
+  })
+});
+
 export const POST = handleAuth(); 
